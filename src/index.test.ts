a1a1d169e1bdaee3f404d19e9162aba10d3fa336
SukkaW <isukkaw@gmail.com>
2026-02-26 01:32:56 +0800

    chore: replace `expect` w/ `earl`

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,21 +1,21 @@
 import { describe, it } from 'mocha';
-import { expect } from 'expect';
+import { expect } from 'earl';
 import { escapeRegexp } from '.';
 
 describe('escapeRegexp', () => {
   it('should work', () => {
-    expect(escapeRegexp(String.raw`\ ^ $ * + ? . ( ) | { } [ ]`)).toBe(String.raw`\\ \^ \$ \* \+ \? \. \( \) \| \{ \} \[ \]`);
+    expect(escapeRegexp(String.raw`\ ^ $ * + ? . ( ) | { } [ ]`)).toEqual(String.raw`\\ \^ \$ \* \+ \? \. \( \) \| \{ \} \[ \]`);
   });
 
   it('escapes `-` in a way compatible with PCRE', () => {
-    expect(escapeRegexp('foo - bar')).toBe(String.raw`foo \x2d bar`);
+    expect(escapeRegexp('foo - bar')).toEqual(String.raw`foo \x2d bar`);
   });
 
   it('escapes `-` in a way compatible with the Unicode flag', () => {
-    expect(new RegExp(escapeRegexp('-'), 'u').test('-')).toBe(true);
+    expect(new RegExp(escapeRegexp('-'), 'u').test('-')).toEqual(true);
   });
 
   it('do not escapes `-` when unicode flag is disabled', () => {
-    expect(escapeRegexp('foo - bar', false)).toBe(String.raw`foo - bar`);
+    expect(escapeRegexp('foo - bar', false)).toEqual(String.raw`foo - bar`);
   });
 });
